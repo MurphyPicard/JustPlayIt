1de2d2858b9c9d532fc116c2462dcc921b97ca4f
murphypicard <ayaghsizian@gmail.com>
2017-01-04 15:36:47 -0500

    made a welcome page and crud works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  root to: 'videos#index'
+  get 'welcome/index'
+
+  root to: 'welcome#index'
 
   resources :videos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
