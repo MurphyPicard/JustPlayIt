18d22b8d98b73097b693fa984531318421420a04
murphypicard <ayaghsizian@gmail.com>
2017-01-04 15:03:56 -0500

    first commit

---
+++ db/migrate/20170104195813_create_videos.rb
@@ -0,0 +1,12 @@
+class CreateVideos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :videos do |t|
+      t.string :artist
+      t.string :song
+      t.string :url
+      t.boolean :lyrics
+
+      t.timestamps
+    end
+  end
+end
